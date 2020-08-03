e8495342c2e48b81571b37f681539bda38b4dde3
lalaalal <bontin0128@gmail.com>
2020-08-04 00:19:06 +0900

    Init

---
+++ Chess/Board.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess
+{
+    public abstract class BoardView
+    {
+        protected Piece[,] board = new Piece[8, 8];
+        public ImmutablePiece this[Point point]
+        {
+            get => board[point.y, point.x];
+        }
+
+        public bool IsEmpty(Point point)
+        {
+            return this[point] == null;
+        }
+
+        public bool IsPathAvailable(Point from, Point to)
+        {
+            if (IsEmpty(from))
+                throw new ArgumentException();
+            Point delta = (to - from).Unit();
+
+            for (Point p = from + delta; p != to; p += delta)
+            {
+                if (!IsEmpty(p))
+                    return false;
+            }
+            return IsEmpty(to) || this[from].IsEnemy(this[to]);
+        }
+
+        public static bool PointInRange(Point point)
+        {
+            return 0 <= point.x && point.x <= 8 && 0 <= point.y && point.y <= 8;
+        }
+    }
+
+    public class Board : BoardView
+    {
+        private new Piece this[Point point]
+        {
+            get => board[point.y, point.x];
+            set => board[point.y, point.x] = value;
+        }
+
+        public void AddPiece(Point point, Team team, Kind kind)
+        {
+            this[point] = PieceFactory.CreatePiece(point, team, kind);
+        }
+
+        public void AddPiece(Piece piece)
+        {
+            Point point = piece.CurrentPoint;
+            this[point] = piece;
+        }
+
+        public bool MovePiece(Point from, Point to)
+        {
+            Piece target = this[from];
+            if (!target.CanMoveTo(to, this))
+                return false;
+
+            this[from] = null;
+            this[to] = target;
+            return true;
+        }
+    }
+}
---
+++ Chess/Point.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess
+{
+    public class Point
+    {
+        public int x { get; private set; }
+        public int y { get; private set; }
+
+        public Point(int x = 0, int y = 0)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Point(Point point)
+        {
+            x = point.x;
+            y = point.y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Move(Point point)
+        {
+            Move(point.x, point.y);
+        }
+        
+        public Point Unit()
+        {
+            int x = this.x == 0 ? 0 : this.x / Math.Abs(this.x);
+            int y = this.y == 0 ? 0 : this.y / Math.Abs(this.y);
+
+            return new Point(x, y);
+        }
+
+        public double Abs()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        public static bool operator==(Point a, Point b)
+        {
+            return a.x == b.x && a.y == b.y;
+        }
+
+        public static bool operator!=(Point a, Point b)
+        {
+            return !(a == b);
+        }
+
+        public static bool operator<(Point a, Point b)
+        {
+            return a.Abs() < b.Abs();
+        }
+
+        public static bool operator>(Point a, Point b)
+        {
+            return a.Abs() > b.Abs();
+        }
+
+        public static Point operator-(Point point)
+        {
+            return new Point(-point.x, -point.y);
+        }
+        public static Point operator+(Point a, Point b)
+        {
+            return new Point(a.x + b.x, a.y + b.y);
+        }
+
+        public static Point operator-(Point a, Point b)
+        {
+            return a + -b;
+        }
+
+        public override string ToString()
+        {
+            return "(" + x + ", " + y + ")";
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is Point point &&
+                   x == point.x &&
+                   y == point.y;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(x, y);
+        }
+    }
+}
